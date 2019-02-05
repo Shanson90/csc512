046e4b6767b30b96104f8cf4cafbf58501f5161c
Shane Williamson <swilliamson@chathamfinancial.com>
2019-02-04 20:11:44 -0500

    better feedback

--- homework/code/src/GuessingGame.java
+++ homework/code/src/GuessingGame.java
@@ -12,7 +12,7 @@ public class GuessingGame {
 
         Random randGen = new Random();
         Integer secretNumber = randGen.nextInt(10);
-        System.out.println("Ok... I'm thinking of a number between 0 and 10");
+        System.out.println("Ok... I'm thinking of a number between 0 and 10.");
         System.out.println("Enter your first guess.");
         Integer guess = keyboard.nextInt();
 
@@ -21,13 +21,24 @@ public class GuessingGame {
             System.exit(0);
         }else{ playUntilWin(guess, secretNumber); }
 
+
         System.out.println("You guessed it!\n" + "It took you " + guessCount + " guesses to guess the secret number " + secretNumber);
+        if(guessCount > 11){
+            System.out.println("You really aren't that smart... There were only 11 possibilities (0 through 10)");
+        }
         System.exit(0);
     }
 
     private static void playUntilWin(Integer guess, Integer secretNumber){
+        String feedback;
         while(!guess.equals(secretNumber)){
-            System.out.println("Nope, sorry! Guess again!");
+            if(guess > secretNumber){
+                feedback = "high";
+            }else{
+                feedback = "low";
+            }
+            System.out.println("Too " + feedback + ", try again.");
+
             if(keyboard.hasNext()){
                 guess = keyboard.nextInt();
                 guessCount++;
