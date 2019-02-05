d845d0785eb5046d087ecf0a6a850986dea77258
Shane Williamson <swilliamson@chathamfinancial.com>
2019-02-04 20:12:34 -0500

    classwork: first method

--- homework/code/src/ShowCar.java
+++ homework/code/src/ShowCar.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class ShowCar {
+
+    public static void main(String[] args){
+        showCar("car", 2);
+    }
+
+    private static void showCar(String s, int position){
+        System.out.println(s.charAt(position));
+    }
 }
