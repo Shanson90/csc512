e297656eb50cc9af4b6bca358c793452677e200e
Shane Williamson <swilliamson@chathamfinancial.com>
2019-02-28 07:14:21 -0500

    Standardize class user names

--- homework/code/src/PayrollMain.java
+++ homework/code/src/PayrollMain.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class RunPayroll {
+public class PayrollMain {
     private static BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws Exception {
