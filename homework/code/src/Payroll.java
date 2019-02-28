4a303a6b16581ca6a76ac38d293eb44bcd2253c2
Shane Williamson <swilliamson@chathamfinancial.com>
2019-02-27 23:11:54 -0500

    Homework 4, Problem 2: Payroll Class

---
+++ homework/code/src/Payroll.java
@@ -0,0 +1,52 @@
+import org.omg.CORBA.portable.ApplicationException;
+
+public class Payroll {
+
+    public int[] employeeId = new int[]{5658845, 4520125, 7895122, 8777541, 8451277, 1302850, 7580489};
+    private int[] hours = new int[7];
+    private double[] payRate = new double[7];
+    private double[] wages = new double[7];
+
+    public double getHours(int eId) throws Exception{
+        int employeeIndex = findEmployeeIndex(eId);
+        return hours[employeeIndex];
+    }
+
+    public void setHours(int eId, int hrs) throws Exception{
+        int employeeIndex = findEmployeeIndex(eId);
+        hours[employeeIndex] = hrs;
+    }
+
+    public double getPayRate(int eId) throws Exception{
+        int employeeIndex = findEmployeeIndex(eId);
+        return payRate[employeeIndex];
+    }
+
+    public void setPayRate(int eId, double rate) throws Exception{
+        if(rate < 6.00){
+            throw new Exception("Pay rate is below minimum wage!");
+        }
+        int employeeIndex = findEmployeeIndex(eId);
+        payRate[employeeIndex] = rate;
+    }
+
+    public double getWages(int eId) throws Exception {
+        int employeeIndex = findEmployeeIndex(eId);
+        return wages[employeeIndex];
+    }
+
+    public void setWages(){
+        for (int i = 0; i < employeeId.length; i++) {
+            wages[i] = hours[i] * payRate[i];
+        }
+    }
+
+    private int findEmployeeIndex(int eId) throws Exception{
+        for(int i = 0; i < employeeId.length; i++){
+            if(employeeId[i] == eId){
+                return i;
+            }
+        }
+        throw new Exception("Employee not found");
+    }
+}
---
+++ homework/code/src/RunPayroll.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class RunPayroll {
+    private static BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        Payroll payroll = new Payroll();
+
+        getData(payroll);
+        payroll.setWages();
+        showReport(payroll);
+    }
+
+    private static void getData(Payroll payroll) throws Exception {
+        for(int empId : payroll.employeeId){
+            System.out.println("\nEmployee ID: " + empId);
+
+            System.out.println("Hours?");
+            int hrs = Integer.parseInt(keyboard.readLine());
+            payroll.setHours(empId, hrs);
+
+            System.out.println("Rate of Pay?");
+            double rate = Double.parseDouble(keyboard.readLine());
+            payroll.setPayRate(empId, rate);
+        }
+    }
+
+    private static void showReport(Payroll payroll) throws Exception {
+        System.out.println("\nEmployee ID  |  Gross Wages");
+        System.out.println("-------------------------");
+
+        for(int empId : payroll.employeeId){
+            System.out.println(empId + "      |  " + payroll.getWages(empId));
+        }
+    }
+}
